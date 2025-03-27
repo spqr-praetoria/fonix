ad3ea1889e620ea9045dbdd45507e8a6cd377697
Benjamin Fowl <b.fowl87@gmail.com>
2025-03-27 12:21:27 +0000

    Add Rails Admin integration and update dependencies
    - Added 'rails_admin' gem to the Gemfile for admin interface management.
    - Updated Gemfile.lock to include 'rails_admin' and its dependencies.
    - Configured routes to mount Rails Admin at '/admin'.
    - Updated package.json and yarn.lock to include 'rails_admin' and related packages.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem "thruster", require: false
 # gem "image_processing", "~> 1.2"
 
 gem "devise", "~> 4.9"
+gem 'rails_admin', '~> 3.0'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   devise_for :users, only: [ :sessions, :registrations, :passwords, :confirmations ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
