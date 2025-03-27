14dcef7a9f8431e8e2bf8cd6d096acaee27a47ae
Benjamin Fowl <b.fowl87@gmail.com>
2025-03-27 12:44:35 +0000

    Refactor seed data setup in db/seeds.rb
    - Commented out user and message creation sections for clarity.
    - Updated channel creation to use Time.now for created_at.
    - Removed cleanup operations for users, channels, and messages.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,44 @@
-# Cleanup
-User.delete_all
-Channel.delete_all
-Message.delete_all
-
-# Create users
-8.times do |i|
-  email = "user_#{i}@example.com"
-  username = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
-
-  FactoryBot.create(:user, email: email, username: username)
-end
-
-puts "Created #{User.count} users"
-
-# Create One channel
 Channel.create!(
   name: "General",
   description: "Mobile payments & interactive services",
-  created_at: Faker::Time.between(from: 1.year.ago, to: Time.current)
+  created_at: Time.now
 )
 
-puts "Created channel"
+# Cleanup
+# User.delete_all
+# Channel.delete_all
+# Message.delete_all
 
-# Create messages
-channel = Channel.first
-users = User.all.to_a
+# # Create users
+# 8.times do |i|
+#   email = "user_#{i}@example.com"
+#   username = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
 
-150.times do |i|
-  Message.create!(
-    content: "Message #{i+1}",
-    user: users.sample,
-    channel: channel,
-    created_at: Faker::Time.between(from: 2.day.ago, to: Time.current)
-  )
-end
+#   FactoryBot.create(:user, email: email, username: username)
+# end
 
-puts "Created #{Message.count} messages"
+# puts "Created #{User.count} users"
+
+# # Create One channel
+# Channel.create!(
+#   name: "General",
+#   description: "Mobile payments & interactive services",
+#   created_at: Faker::Time.between(from: 1.year.ago, to: Time.current)
+# )
+
+# puts "Created channel"
+
+# # Create messages
+# channel = Channel.first
+# users = User.all.to_a
+
+# 150.times do |i|
+#   Message.create!(
+#     content: "Message #{i+1}",
+#     user: users.sample,
+#     channel: channel,
+#     created_at: Faker::Time.between(from: 2.day.ago, to: Time.current)
+#   )
+# end
+
+# puts "Created #{Message.count} messages"
