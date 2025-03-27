c4b65cf3e29258ceea186ecd71fb5a7db9824345
Benjamin Fowl <b.fowl87@gmail.com>
2025-03-27 06:28:28 +0000

    Refactor DateTimePresenter to remove inheritance from SimpleDelegator
    - Simplify DateTimePresenter by removing inheritance from SimpleDelegator.
    - Maintain functionality for UK-formatted date output.

--- app/presenters/date_time_presenter.rb
+++ app/presenters/date_time_presenter.rb
@@ -1,4 +1,4 @@
-class DateTimePresenter < SimpleDelegator
+class DateTimePresenter
   # UK-formatted date with words (e.g., "5 March 2024 at 2:30 PM")
   def uk_formatted
     datetime.in_time_zone("London").strftime("%e %B %Y at %l:%M %p")
