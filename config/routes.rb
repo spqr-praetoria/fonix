65464e83dc6670947827675d9eb03ea3fe831ed8
Benjamin Fowl <b.fowl87@gmail.com>
2025-03-27 13:16:39 +0000

    Remove Rails Admin integration and related configuration files
    - Deleted the Rails Admin mount from routes.
    - Removed Rails Admin initializer and associated credentials for development and test environments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   devise_for :users, only: [ :sessions, :registrations, :passwords, :confirmations ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
