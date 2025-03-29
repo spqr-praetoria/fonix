623fe6e344ef7277d3deab2818c687d617cce0bb
Benjamin Fowl <b.fowl87@gmail.com>
2025-03-29 16:57:58 +0000

    Remove unused PasswordVisibility import from application.js

--- app/frontend/javascript/controllers/application.js
+++ app/frontend/javascript/controllers/application.js
@@ -3,7 +3,6 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 import { Alert } from "tailwindcss-stimulus-components"
-import PasswordVisibility from '@stimulus-components/password-visibility'
 
 application.register("alert", Alert)
 application.register('password-visibility', PasswordVisibility)
